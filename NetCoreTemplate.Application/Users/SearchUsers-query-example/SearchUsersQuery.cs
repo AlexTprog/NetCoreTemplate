0c6845d8f3cf00afb71556479a09d6be6207c4a8
Alexander Torre <alexander.torre.arteaga@gmail.com>
2024-10-27 13:31:47 -0500

    remove user query example

--- NetCoreTemplate.Application/Users/SearchUsers-query-example/SearchUsersQuery.cs
+++
@@ -1,7 +0,0 @@
-﻿using NetCoreTemplate.Application.Abstractions.Messaging;
-using NetCoreTemplate.Application.Users.SearchUsers;
-
-namespace NetCoreTemplate.Application.Users.SearchUser
-{
-    public sealed record SearchUsersQuery() : IQuery<IReadOnlyList<UserResponse>>;
-}
